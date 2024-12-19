5434b63f7efad0dec7b1ff9be5c0be76679cdde8
Satyam Sah <satnarayan5166@gmail.com>
2024-12-19 14:55:05 +0545

    connected url with environment

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,8 +1,8 @@
 import UserData from "../plugin/useUserData";
 
-export const API_BASE_URL = "http://127.0.0.1:8000/api/";
-export const SERVER_URL = "http://127.0.0.1:8000";
-export const CLIENT_URL = "http://localhost:5173";
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "/api/";
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL || "";
+export const CLIENT_URL = import.meta.env.VITE_CLIENT_URL || "";
 export const PAYPAL_CLIENT_ID = "test";
 export const CURRENCY_SIGN = "$";
 export const userId = UserData()?.user_id;
